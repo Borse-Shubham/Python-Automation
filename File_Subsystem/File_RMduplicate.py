7b3087329f900ddaae8d28deb241b34d960dbdb9
Borse-Shubham <shubhamborse0911@gmail.com>
2023-10-02 18:49:48 +0530

    Log File Creation

--- File_Subsystem/File_RMduplicate.py
+++ File_Subsystem/File_RMduplicate.py
@@ -1,11 +1,13 @@
 from sys import *
-import os import hashlib
+import os
+import hashlib
 import time
 
 def DeleteFiles(dict1): 
     results = list(filter(lambda x: len(x) > 1, dict1.values()))
 
     icnt = 0
+    iFound = 0
 
     if len(results) > 0:
         for result in results: 
@@ -13,20 +15,22 @@ def DeleteFiles(dict1):
                 icnt+=1
                 if icnt >= 2: 
                     os.remove(subresult)
+                    iFound+=1
             icnt = 0
+        print("The Number of Files Deleted : ",iFound)
     else:
         print("No duplicate files found.")
 
 def hashfile(path, blocksize = 1024):
 
         afile = open(path, 'rb') 
-        hasher hashlib.md5()
+        hasher = hashlib.md5()
 
         buf = afile.read(blocksize)
 
         while len(buf) > 0:
             hasher.update(buf)
-            buf afile.read(blocksize)
+            buf = afile.read(blocksize)
         afile.close()
         return hasher.hexdigest()
 
@@ -47,13 +51,15 @@ def findDup(path):
                 for filen in fileList:
 
                     path = os.path.join(dirName, filen)
-                    file_hash hashfile(path)
+                    file_hash = hashfile(path)
 
-                    if file_hash in dups: dups[file_hash].append(path)
-                        else: dups[file_hash] = [path]
-        return dups
-    else:
-        print("Invalid Path")
+                    if file_hash in dups: 
+                        dups[file_hash].append(path)
+                    else: 
+                        dups[file_hash] = [path]
+            return dups
+        else:
+            print("Invalid Path")
 
 def printResults(dict1):
 
